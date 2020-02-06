1a0091844e433c0a6c6d45f22f61d71318e1121b
Cindy Le <lecindy95@gmail.com>
2020-02-06 15:22:01 -0800

    instatiated PageHeader.

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-var tableElement = document.querySelector("tbody")
+var tableElement = document.querySelector("tbody");
+var newGradeTable = new GradeTable(tableElement);
 
-var newGradeTable = new GradeTable(tableElement)
+var headerElement = document.querySelector("h3");
+var newPageHeader = new PageHeader(headerElement);
+
 var newApp = new App(newGradeTable);
 
 newApp.start();
