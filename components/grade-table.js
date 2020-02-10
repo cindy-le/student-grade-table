0e26490c0b66304381c62ae605c264586c4e78d9
Cindy Le <lecindy95@gmail.com>
2020-02-09 17:49:20 -0800

    created button element, eventlistener. Updated gradetable loop.

--- components/grade-table.js
+++ components/grade-table.js
@@ -6,18 +6,53 @@ class GradeTable {
     var tbody = document.querySelector("tbody");
     tbody.textContent = "";
     for (var i = 0; i < grades.length; i++) {
-      var row = document.createElement("tr");
-      var name = document.createElement("td");
-      name.textContent = grades[i].name;
-      var course = document.createElement("td");
-      course.textContent = grades[i].course;
-      var grade = document.createElement("td");
-      grade.textContent = grades[i].grade;
-      row.append(name, course, grade);
-      tbody.appendChild(row)
+      this.renderGradeRow(grades[i], this.deleteGrade);
+      // var row = document.createElement("tr");
+      // var name = document.createElement("td");
+      // name.textContent = grades[i].name;
+      // var course = document.createElement("td");
+      // course.textContent = grades[i].course;
+      // var grade = document.createElement("td");
+      // grade.textContent = grades[i].grade;
+      // row.append(name, course, grade);
+      // tbody.appendChild(row)
     }
   }
   onDeleteClick(deleteGrade) {
     this.onDeleteClick = deleteGrade;
+  }
+  renderGradeRow(data, deleteGrade) {
+    var tbodyEl = document.querySelector("tbody");
+    var row = document.createElement("tr");
+    var name = document.createElement("td");
+    name.textContent = data.name;
+    var course = document.createElement("td");
+    course.textContent = data.course;
+    var grade = document.createElement("td");
+    grade.textContent = data.grade;
+    var buttonTd = document.createElement("td");
+    var button = document.createElement("button");
+    button.setAttribute("class", "btn btn-danger");
+    button.textContent = "DELETE";
+    button.addEventListener("click", function (){
+      deleteGrade(data.id)
+    });
+    buttonTd.appendChild(button);
+    row.append(name, course, grade, buttonTd);
+    tbodyEl.appendChild(row);
+    return row;
+
+
+    // var tbody = document.querySelector("tbody")
+    // var tableRow = document.createElement("tr");
+    // var tableData = document.createElement("td");
+    // var button = document.createElement("button");
+    // button.textContent = "DELETE";
+    // button.setAttribute(class, "btn btn-danger");
+    // tableData.textContent = button;
+    // tableRow.appendChild(tableData);
+    // tbody.append(tableRow);
+    // button.addEventListener("click", deleteGrade(data.id));
+    // return tableRow;
   }
 }
